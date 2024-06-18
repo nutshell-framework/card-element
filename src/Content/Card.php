a664bc91fa85b4dd00e1d1e210351e90f910fa67
Dennis Erdmann <dennis@erdmann-freunde.de>
2024-06-18 13:58:12 +0200

    fix error, when card text is not set

--- src/Content/Card.php
+++ src/Content/Card.php
@@ -35,7 +35,7 @@ class Card extends ContentElement
     protected function compile()
     {
         // Add the static files URL to images
-        if ($staticUrl = System::getContainer()->get('contao.assets.files_context')->getStaticUrl()) {
+        if ($this->text && $staticUrl = System::getContainer()->get('contao.assets.files_context')->getStaticUrl()) {
             $path = Config::get('uploadPath') . '/';
             $this->text = str_replace(' src="' . $path, ' src="' . $staticUrl . $path, $this->text);
         }
